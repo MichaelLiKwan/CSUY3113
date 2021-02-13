ff1419006a792dc14915b96d14d43c7b08c7936e
Michael Kwan <mk6147@nyu.edu>
2021-02-12 22:50:37 -0500

    finished proj 1

--- VisualStudioSDLProject/SDLProject/main.cpp
+++ VisualStudioSDLProject/SDLProject/main.cpp
@@ -11,19 +11,50 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
 ShaderProgram program;
-glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix, playerMatrix, sunMatrix;
 
-float player1_x = 0;
-float player1_rotate = 0;
-float player2_x = 0;
+float player_x = 0;
+float player_rotate = 180;
+bool player_faceRight = true;
+
+float sun_x = -3;
+float sun_y = 2.5;
+float sun_rotate = 0;
+
+GLuint playerTextureID;
+GLuint sunTextureID;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    stbi_image_free(image);
+    return textureID;
+}
 
 void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Triangle!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -33,19 +64,28 @@ void Initialize() {
 
     glViewport(0, 0, 640, 480);
 
-    program.Load("shaders/vertex.glsl", "shaders/fragment.glsl");
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
 
     viewMatrix = glm::mat4(1.0f);
-    modelMatrix = glm::mat4(1.0f);
+    //modelMatrix = glm::mat4(1.0f);
+    playerMatrix = glm::mat4(1.0f);
+    sunMatrix = glm::mat4(1.0f);
     projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
     program.SetProjectionMatrix(projectionMatrix);
     program.SetViewMatrix(viewMatrix);
-    program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
 
     glUseProgram(program.programID);
 
-    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+
+    glEnable(GL_BLEND);
+    // Good setting for transparency
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    playerTextureID = LoadTexture("knight.png");
+    sunTextureID = LoadTexture("sun.png");
 }
 
 void ProcessInput() {
@@ -63,26 +103,60 @@ void Update() {
     float ticks = (float)SDL_GetTicks() / 1000.0f;
     float deltaTime = ticks - lastTicks;
     lastTicks = ticks;
+    
+    if (player_faceRight == true) {
+        player_x += 1.0f * deltaTime;
+        if (player_x >= 3) {
+            player_faceRight = false;
+        }
+    }
+    else if (player_faceRight == false) {
+        player_x -= 1.0f * deltaTime;
+        if (player_x <= -3) {
+            player_faceRight = true;
+        }
+    }
 
-    player1_x += 1.0f * deltaTime;
-    player1_rotate += 90.0f * deltaTime;
+    //player_rotate += 90.0f * deltaTime;
 
-    modelMatrix = glm::mat4(1.0f);
-    modelMatrix = glm::translate(modelMatrix, glm::vec3(player1_x, 0.0f, 0.0f));
-    modelMatrix = glm::rotate(modelMatrix, glm::radians(player1_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
+    playerMatrix = glm::mat4(1.0f);
+    playerMatrix = glm::translate(playerMatrix, glm::vec3(player_x, 0.0f, 0.0f));
+    //playerMatrix = glm::rotate(playerMatrix, glm::radians(player_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
+    if (player_faceRight == true) {
+        playerMatrix = glm::rotate(playerMatrix, glm::radians(player_rotate), glm::vec3(0.0f, 1.0f, 0.0f));
+    }
+        
 
+    //sun_x += 1.0f * deltaTime;
+    sun_rotate += 90.0f * deltaTime;
+
+    sunMatrix = glm::mat4(1.0f);
+    sunMatrix = glm::translate(sunMatrix, glm::vec3(sun_x, sun_y, 0.0f));
+    sunMatrix = glm::rotate(sunMatrix, glm::radians(sun_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
+    sunMatrix = glm::scale(sunMatrix, glm::vec3(2.0f, 2.0f, 1.0f));
 }
 
 void Render() {
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
     glClear(GL_COLOR_BUFFER_BIT);
 
-    program.SetModelMatrix(modelMatrix);
-
-    float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
     glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
     glEnableVertexAttribArray(program.positionAttribute);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+
+    program.SetModelMatrix(playerMatrix);
+    glBindTexture(GL_TEXTURE_2D, playerTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    program.SetModelMatrix(sunMatrix);
+    glBindTexture(GL_TEXTURE_2D, sunTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
     glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
 
     SDL_GL_SwapWindow(displayWindow);
 }
